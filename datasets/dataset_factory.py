58c5c05e53f087ec4d866fda7c0dfd80e7bb0653
rvjst <90255680+rvjst@users.noreply.github.com>
2024-06-21 15:56:02 +0900

    Add files via upload

---
+++ datasets/dataset_factory.py
@@ -0,0 +1,18 @@
+from .srn import SRNDataset
+#from .co3d import CO3DDataset
+#from .nmr import NMRDataset
+from .objaverse import ObjaverseDataset
+from .gso import GSODataset
+
+def get_dataset(cfg, name):
+    # cfg.data.category = "objaverse"
+    if cfg.data.category == "cars" or cfg.data.category == "chairs":
+        return SRNDataset(cfg, name)
+    elif cfg.data.category == "hydrants" or cfg.data.category == "teddybears":
+        return CO3DDataset(cfg, name)
+    elif cfg.data.category == "nmr":
+        return NMRDataset(cfg, name)
+    elif cfg.data.category == "objaverse":
+        return ObjaverseDataset(cfg, name)
+    elif cfg.data.category == "gso":
+        return GSODataset(cfg, name)
